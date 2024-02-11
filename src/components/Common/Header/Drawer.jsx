2a838bfb4fdcfcee1837b30b98d266f28ddf132e
aditya4-4 <adityadurgesh4@gmail.com>
2024-02-11 21:24:08 +0530

    update the drawer component

--- src/components/Common/Header/Drawer.jsx
+++ src/components/Common/Header/Drawer.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import Drawer from "@mui/material/Drawer";
 import Button from "@mui/material/Button";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import ThemeSwitch from "../ToggleSwitch";
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = useState(false);
@@ -14,6 +15,7 @@ export default function TemporaryDrawer() {
       </Button>
       <Drawer anchor={"right"} open={open} onClose={() => setOpen(false)}>
         <div className="drawer-link">
+          <ThemeSwitch />
           <Link to="/">
             <p className="link">Home</p>
           </Link>
--- src/components/Common/Header/index.jsx
+++ src/components/Common/Header/index.jsx
@@ -9,11 +9,11 @@ function Header() {
     <div className='navbar'>
       <h1 className='logo'>CryptoTracker<span style={{color : '{var(--blue)}'}}>.</span></h1>
       <div className='links'>
-        <ThemeSwitch />
+        <ThemeSwitch/>
         <Link to="/"><p className='link'>Home</p></Link>
         <Link to="/compare"><p className='link'>Compare</p></Link>
         <Link to="/watchlist"><p className='link'>Watchlist</p></Link>
-        <Link to="/dashboard"><Button text={"Dashboard"} onClick={() => console.log()} /></Link>
+        <Link to="/dashboard"><Button  text={"Dashboard"} onClick={() => console.log()} /></Link>
       </div>
       <div className="mobile-drawer">
         <TemporaryDrawer />
